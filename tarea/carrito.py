677aeb539438c622cb2f56407a85a893e4adbbe3
AgosRobledo <131928579+AgosRobledo@users.noreply.github.com>
2023-06-14 10:44:00 -0300

    cambios a las opciones, agrego funciones

--- tarea/carrito.py
+++ tarea/carrito.py
@@ -1,6 +1,10 @@
+import os
+
 salir=False
+datos=0
+cod=0
 productos= {
-    "1" : {
+    "11" : {
         "nombre" : "Air Force 1",
         "marca": "nike",
         "precio": 5800.50,
@@ -8,7 +12,7 @@ productos= {
         "talle": "35",
 
     },
-    "2" : {
+    "12" : {
         "nombre" : "Dunk",
         "marca": "adidas",
         "precio": 20000.30,
@@ -16,7 +20,7 @@ productos= {
         "talle": "36",
 
     },
-    "3" : {
+    "13" : {
         "nombre" : "Air Max 90",
         "marca": "Nike",
         "precio": 18000.50,
@@ -24,33 +28,79 @@ productos= {
         "talle": "35",
 
     },
-    "4" : {
-        "nombre" : "",
-        "marca": "",
-        "precio": "",
-        "stock": "",
-        "caracteristicas": "",
+    "14" : {
+        "nombre" : "ARCADE LOW",
+        "marca": "Fila",
+        "precio": 15000,
+        "stock": 7,
+        "talle": "40",
 
     },
-    "5" : {
-        "nombre" : "",
-        "marca": "",
-        "precio": "",
-        "stock": "",
-        "caracteristicas": "",
+    "15" : {
+        "nombre" : "AIR NFH",
+        "marca" : "Jordan",
+        "precio" : 20000,
+        "stock" : 15,
+        "talle" : "40",
 
-    },
-    "6" : {
-        "nombre" : "",
-        "marca": "",
-        "precio": "",
-        "stock": "",
-        "caracteristicas": "",
+},
+    "16" : {
+        "nombre" : "ORIGINALS SUPERSTAR",
+        "marca": "Adidas",
+        "precio": 16000,
+        "stock": 8,
+        "talle": "38",
 
     },
 }
+carrito={}
 
+primer_producto = productos["11"]
+segundo_producto = productos["12"]
+tercero_producto = productos["13"]
+cuarto_producto = productos["14"]
+quinto_producto = productos["15"]
+sexto_producto = productos["16"]
 
+def mostrar_productos(productos):
+    print ("Los productos que ofrecemos son: ")
+    for key, value in productos.items():
+        
+        print("Nombre:", productos[key]["nombre"])
+        print("Marca:", productos[key]["marca"])
+        print("Precio: $", productos[key]["precio"])
+        print("Stock:", productos[key]["stock"])
+        print("Talle:", productos[key]["talle"])
+        print()
+
+def buscarProducto(cod, productos):
+    while(True):
+        for key, value in productos.items():
+
+            if cod==key:
+                
+                print("")
+                print ("Producto encontrado:")
+                print("Nombre:", productos[key]["nombre"])
+                print("Marca:", productos[key]["marca"])
+                print("Precio: $", productos[key]["precio"])
+                print("Stock:", productos[key]["stock"])
+                print("Talle:", productos[key]["talle"])
+                print("")
+                return
+        cod = input("El producto que desea buscar no existe, vuelva a introducir otro codigo: ")
+
+def validar_opcion(opcion):
+    while(True):
+        if opcion.isnumeric():
+            opcion =int(opcion)
+            if opcion<5 and opcion>0:
+                return opcion
+            else:
+                opcion= input("La opcion ingresada no es correcta, vuelva a ingresar otra opción: ")    
+            #validar que las primeras opciones sean correctas
+        else:
+            opcion= input("La opcion ingresada no es un numero, vuelva a ingresar otra opción: ")
 
 
 
@@ -65,7 +115,48 @@ while (salir==False):
     4)finalizar compra
     5)salir
     """)
-    opcion= int(input("Ingrese una opción: "))
-    if (opcion==1):
-        print (productos.items())
+    
+    opcion= validar_opcion(input("Ingrese una opción: "))
+    
+    #como limpiar pantalla
+    if opcion == 1:
+        print()
+        mostrar_productos(productos)
+    if opcion ==2:
+        buscarProducto(input("Ingrese el codigo del producto a buscar: "),productos)
+        #asi llamo una función
+        continuar = input("Ingrese ENTER para continuar")
+        os.system("cls")
+    
 
+    if opcion == 3:
+        cod = input ("Ingrese el producto que desea agregar a su carrito ")
+        cantidad =int(input("Cantidad:"))
+        #agregar producto a un nuevo diccionario
+        #y 
+
+
+
+
+"""#funcion para mostrar los datos del diccionario en pantalla
+def imprimir_datos(productos):
+    for key in productos.items():
+        print("Nombre:",key)
+        print("Marca:",productos[key]["marca"])
+        print("Precio: $",productos[key]["precio"])
+        print("Stock:",productos[key]["stock"])
+        print("Talle:",productos[key]["talle"])
+        print()
+        imprimir_datos(productos)
+        
+#preguntar como hago para llamar a una función
+
+def imprimir_lista(datos):
+    for i in range (len(datos)):
+
+        print("Nombre: ", datos[i]["nombre"])
+        print("Marca: ", datos[i]["marca"])
+        print("Precio: $", datos[i]["precio"])
+        print("Stock: ", datos[i]["stock"])
+        print("Talle: ", datos[i]["talle"])
+        imprimir_lista(datos)"""
